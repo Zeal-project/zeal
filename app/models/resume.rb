4d9311c08e6145b8b5c64826a5f2c38dbd7e3405
YengTsan <Yengttt@gmail.com>
2016-07-12 01:59:10 +0800

    user can create and view the resume

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -5,7 +5,8 @@ class Resume < ApplicationRecord
   enum ideal_role: [ :engineer, :product_design, :businesses_dev, :marketing ]
   enum job_search_status: [ :strong_active, :active, :passive ]
   enum exp_in_role: [ :training, :entry_level, :experienced_hire, :manager, :director, :executive ]
-  enum length_of_working_exp: [ "0_1", "1_3", "4_6", "7-10", "above_10" ]
+  enum length_of_working_exp: [ "below_one", "one_three", "four_six", "seven_ten", "above_ten" ]
+  enum ideal_salary: ["fouty_sixty", "sixty_eighty", "eighty_hundrued", "above_hundrued" ]
 
   belongs_to :user
 
