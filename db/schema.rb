d0f9f6c953a420ea39cf1695ecf9a1908fcd9d5b
TzuYuan <tzuyuanliu0830@gmail.com>
2016-07-16 18:33:57 +0800

    adjust to newer schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713121041) do
+ActiveRecord::Schema.define(version: 20160714083027) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "career_posts", force: :cascade do |t|
@@ -70,10 +71,10 @@ ActiveRecord::Schema.define(version: 20160713121041) do
     t.datetime "updated_at",                          null: false
     t.string   "fb_token"
     t.string   "fb_uid"
+    t.string   "role"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["fb_uid"], name: "index_users_on_fb_uid", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.string   "role"
   end
 
 end
