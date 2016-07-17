ad9047a7a63ef8f263fd68b0f49e1e347838b140
洪璽凱 <tpanakin@gmail.com>
2016-07-17 20:51:01 +0800

    tmp

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20160713121041) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "career_posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_career_posts_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_career_posts_on_user_id"
   end
 
   create_table "companies", force: :cascade do |t|
@@ -31,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160713121041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_companies_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
   create_table "jobs", force: :cascade do |t|
@@ -41,7 +38,7 @@ ActiveRecord::Schema.define(version: 20160713121041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "company_id"
-    t.index ["company_id"], name: "index_jobs_on_company_id", using: :btree
+    t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
   create_table "resumes", force: :cascade do |t|
@@ -71,9 +68,9 @@ ActiveRecord::Schema.define(version: 20160713121041) do
     t.datetime "updated_at",                          null: false
     t.string   "fb_token"
     t.string   "fb_uid"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["fb_uid"], name: "index_users_on_fb_uid", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["fb_uid"], name: "index_users_on_fb_uid"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
