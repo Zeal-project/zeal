5484f09948f3d6278234241c93575d484ba7a288
洪璽凱 <tpanakin@gmail.com>
2016-07-20 16:06:56 +0800

    user_can_share_career_post_on_facebook

--- app/helpers/career_posts_helper.rb
+++ app/helpers/career_posts_helper.rb
@@ -1,2 +1,5 @@
 module CareerPostsHelper
+	def fb_share_career_post(career_post)
+    '<iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2F103.3.62.242' + '/career_posts/' + career_post.id.to_s + '%2F&layout=button&size=small&mobile_iframe=true&appId=1728317947409516&width=52&height=20" width="52" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'
+  end
 end
