1c4f4778648f1887ea30286795e5ba55cdd249aa
洪璽凱 <tpanakin@gmail.com>
2016-07-26 15:19:15 +0800

    user_can_share_career_post_by_email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+	default :from => "zealor.tw@gmail.com"
+	
+	def recommend_career_post_email(career_post, user)
+  	@user = user
+  	@url  = 'http://103.3.62.242/'+'/career_posts/'+ career_post.id.to_s
+  	#@url  = 'http://localhost:3000/'+'/career_posts/'+ career_post.id.to_s
+  	mail( subject: '推薦你一篇好文章！', bcc: "zealor.tw@gmail.com" )
+  end
+
+end
+
