9e3f0c95f7413049ab5b0acf2b33e1509c24c945
YengTsan <Yengttt@gmail.com>
2016-07-29 16:01:29 +0800

    transfer sidebar from every page to partial

--- app/helpers/hr_sys/recruit_helper.rb
+++ app/helpers/hr_sys/recruit_helper.rb
@@ -1,2 +1,16 @@
 module HrSys::RecruitHelper
+  def side_bar_link(action, icon, name)
+    ('<li ' + active?(params, action) +
+        '><a href="' + action + '"
+            ><i class="' + icon + '"></i>
+            <span class="menu-text">' + name + '</span>
+            <span class="selected"></span>
+        </a>
+    </li>').html_safe
+  end
+
+  def active?(params, action)
+    return 'class="active"' if params.values.second == action.split('.').first
+    ""
+  end
 end
